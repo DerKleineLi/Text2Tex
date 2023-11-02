9e16644f8805f96cd341f18143f612ac24f7671b
Haoxuan Li <haoxuan.li@tum.de>
2023-11-02 11:35:00 +0100

    update for new data organization

--- combine.py
+++ combine.py
@@ -1,13 +1,29 @@
+from argparse import ArgumentParser
 from pathlib import Path
 
 import cv2
 
-scene_name = "LivingRoom-36282"
-short_prompt = "classic"  # used for output_dir
+# scene_name = "LivingRoom-36282"
+# short_prompt = "classic"  # used for output_dir
+
+
+def parse_args():
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "--scene_name", type=str, default="0b16abb1-4a59-4ce3-85b5-8ec10440d9dd"
+    )
+    arg_parser.add_argument("--short_prompt", type=str, default="classic")
+    args = arg_parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
+    args = parse_args()
+    scene_name = args.scene_name
+    short_prompt = args.short_prompt
+
     input_dir = Path("data") / scene_name
-    output_dir = Path("outputs") / scene_name / short_prompt
+    output_dir = Path("outputs") / "text2tex" / scene_name / short_prompt
 
     # input_stuff_dir = input_dir / "stuff"
 
@@ -25,21 +41,17 @@ if __name__ == "__main__":
     result = cv2.imread(str(output_stuff_file))
 
     input_things_dir = input_dir / "things"
-    png_files = list(input_things_dir.glob("*.png"))
-    for png_file in png_files:
-        mask_png = cv2.imread(str(png_file))
+    output_things_dir = output_dir / "things"
+    things_dirs = list(output_things_dir.glob("*"))
+    for thing_dir in things_dirs:
+        mask_file = thing_dir / "42-p36-h0-1.0-0.3-0.1/generate/mesh/9_texture_mask.png"
+        texture_file = thing_dir / "42-p36-h0-1.0-0.3-0.1/generate/mesh/9.png"
+        mask_png = cv2.imread(str(mask_file))
         mask = mask_png[:, :, 0] > 128
-        thing_name = png_file.stem
-        output_png_file = (
-            output_dir
-            / "things"
-            / thing_name
-            / "42-p36-h0-1.0-0.3-0.1"
-            / "generate"
-            / "mesh"
-            / "9.png"
-        )
-        output_png = cv2.imread(str(output_png_file))
-        result[mask] = output_png[mask]
-
-    cv2.imwrite(str(output_dir / "texture.png"), result)
+        texture_png = cv2.imread(str(texture_file))
+        result[mask] = texture_png[mask]
+    texture_file = (
+        Path("outputs") / "text2tex" / f"text2tex_{scene_name}_{short_prompt}.png"
+    )
+    cv2.imwrite(str(texture_file), result)
+    print("texture written to ", texture_file)
--- create_jobs.py
+++ create_jobs.py
@@ -1,29 +1,31 @@
 from pathlib import Path
 
-scene_name = "LivingRoom-36282"
+scene_name = "0b16abb1-4a59-4ce3-85b5-8ec10440d9dd"
 short_prompt = "classic"  # used for output_dir
 prompt_stuff = "a classic style living room"
 prompt_thing = "a {thing_type} in a classic style living room"
 camera_xyz = [0.85963, 1.1377, -4.6706]
 radius = 2.5
 
-stuff_pattern_file = "jobs/pattern/stuff.job"
-thing_pattern_file = "jobs/pattern/thing.job"
-
-with open(stuff_pattern_file, "r") as f:
-    stuff_pattern = f.read()
-with open(thing_pattern_file, "r") as f:
-    thing_pattern = f.read()
-
 if __name__ == "__main__":
-    job_dir = Path("jobs") / scene_name / short_prompt
-    input_dir = Path("data") / scene_name
-    output_dir = Path("outputs") / scene_name / short_prompt
+    stuff_pattern_file = "jobs/pattern/stuff.job"
+    thing_pattern_file = "jobs/pattern/thing.job"
+    combine_pattern_file = "jobs/pattern/combine.job"
+    with open(stuff_pattern_file, "r") as f:
+        stuff_pattern = f.read()
+    with open(thing_pattern_file, "r") as f:
+        thing_pattern = f.read()
+    with open(combine_pattern_file, "r") as f:
+        combine_pattern = f.read()
 
-    job_dir.mkdir(parents=True, exist_ok=True)
+    job_dir = Path("jobs")
+    scene_dir = Path("data/3D-FRONT_preprocessed/scenes") / scene_name
+    room_dir = next(scene_dir.iterdir())
+    input_dir = room_dir / "meshes"
+    output_dir = Path("outputs") / "text2tex" / scene_name / short_prompt
 
-    stuff_job = stuff_pattern.format(
-        scene_name=scene_name,
+    job = stuff_pattern.format(
+        input_dir=input_dir / "stuff",
         output_dir=output_dir / "stuff",
         prompt=prompt_stuff,
         radius=radius,
@@ -32,23 +34,21 @@ if __name__ == "__main__":
         camera_center_z=camera_xyz[2],
     )
 
-    with open(job_dir / "stuff.job", "w") as f:
-        f.write(stuff_job)
-
     input_things_dir = input_dir / "things"
-    png_files = list(input_things_dir.glob("*.png"))
-    for png_file in png_files:
-        thing_name = png_file.stem
+    obj_files = list(input_things_dir.glob("*.obj"))
+    for obj_file in obj_files:
+        thing_name = obj_file.stem
         thing_type = thing_name.split("_")[1]
         thing_job = thing_pattern.format(
+            input_dir=input_things_dir,
             scene_name=scene_name,
             thing_name=thing_name,
             output_dir=output_dir / "things" / thing_name,
             prompt=prompt_thing.format(thing_type=thing_type),
         )
-        with open(job_dir / f"{thing_name}.job", "w") as f:
-            f.write(thing_job)
+        job += thing_job
 
-    with open(job_dir / "submit.sh", "w") as f:
-        f.write("#!/bin/bash\n")
-        f.write(f"for i in *.job; do\n    sbatch -p submit $i;\ndone")
+    job += combine_pattern.format(scene_name=scene_name, short_prompt=short_prompt)
+
+    with open(job_dir / f"{scene_name}_{short_prompt}.job", "w") as f:
+        f.write(job)
